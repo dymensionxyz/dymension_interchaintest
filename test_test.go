97371dc8fd33f6f5a9c5f078f6d94ddde484f111
Itay Levy <itaylevy@Itays-MacBook-Pro.local>
2023-11-01 14:05:35 +0200

    test works

--- test_test.go
+++ test_test.go
@@ -1,17 +1,27 @@
 package dymension_interchaintest
 
 import (
+	"context"
+	"cosmossdk.io/math"
+	"fmt"
+	transfertypes "github.com/cosmos/ibc-go/v8/modules/apps/transfer/types"
+	clienttypes "github.com/cosmos/ibc-go/v8/modules/core/02-client/types"
 	"github.com/strangelove-ventures/interchaintest/v8"
+	"github.com/strangelove-ventures/interchaintest/v8/chain/cosmos"
 	"github.com/strangelove-ventures/interchaintest/v8/ibc"
+	"github.com/strangelove-ventures/interchaintest/v8/testreporter"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap/zaptest"
 	"testing"
+	"time"
 )
 
 func TestLearn(t *testing.T) {
 	if testing.Short() {
 		t.Skip("skipping in short mode")
 	}
+	ctx := context.Background()
+
 	t.Parallel()
 	cf := interchaintest.NewBuiltinChainFactory(zaptest.NewLogger(t), []*interchaintest.ChainSpec{
 		{Name: "gaia", Version: "v7.0.0", ChainConfig: ibc.ChainConfig{
@@ -26,7 +36,7 @@ func TestLearn(t *testing.T) {
 	r := interchaintest.NewBuiltinRelayerFactory(ibc.CosmosRly, zaptest.NewLogger(t)).Build(
 		t, client, network)
 	const ibcPath = "gaia-osmo-demo"
-	_ = interchaintest.NewInterchain().
+	ic := interchaintest.NewInterchain().
 		AddChain(gaia).
 		AddChain(osmosis).
 		AddRelayer(r, "relayer").
@@ -36,5 +46,82 @@ func TestLearn(t *testing.T) {
 			Relayer: r,
 			Path:    ibcPath,
 		})
+	// Log location
+	f, err := interchaintest.CreateLogFile(fmt.Sprintf("%d.json", time.Now().Unix()))
+	require.NoError(t, err)
+	// Reporter/logs
+	rep := testreporter.NewReporter(f)
+	eRep := rep.RelayerExecReporter(t)
 
+	// Build interchain
+	require.NoError(t, ic.Build(ctx, eRep, interchaintest.InterchainBuildOptions{
+		TestName:  t.Name(),
+		Client:    client,
+		NetworkID: network,
+		// BlockDatabaseFile: interchaintest.DefaultBlockDatabaseFilepath(),
+
+		SkipPathCreation: false},
+	),
+	)
+
+	// Create and Fund User Wallets
+	fundAmount := math.NewInt(10_000_000)
+	users := interchaintest.GetAndFundTestUsers(t, ctx, "default", fundAmount.Int64(), gaia, osmosis)
+	gaiaUser := users[0]
+	osmosisUser := users[1]
+
+	gaiaUserBalInitial, err := gaia.GetBalance(ctx, gaiaUser.FormattedAddress(), gaia.Config().Denom)
+	require.NoError(t, err)
+	require.True(t, gaiaUserBalInitial.Equal(fundAmount))
+
+	// Get Channel ID
+	gaiaChannelInfo, err := r.GetChannels(ctx, eRep, gaia.Config().ChainID)
+	require.NoError(t, err)
+	gaiaChannelID := gaiaChannelInfo[0].ChannelID
+
+	osmoChannelInfo, err := r.GetChannels(ctx, eRep, osmosis.Config().ChainID)
+	require.NoError(t, err)
+	osmoChannelID := osmoChannelInfo[0].ChannelID
+
+	height, err := osmosis.Height(ctx)
+	require.NoError(t, err)
+
+	// Send Transaction
+	amountToSend := math.NewInt(1_000_000)
+	dstAddress := osmosisUser.FormattedAddress()
+	transfer := ibc.WalletAmount{
+		Address: dstAddress,
+		Denom:   gaia.Config().Denom,
+		Amount:  amountToSend,
+	}
+	tx, err := gaia.SendIBCTransfer(ctx, gaiaChannelID, gaiaUser.KeyName(), transfer, ibc.TransferOptions{})
+	require.NoError(t, err)
+	require.NoError(t, tx.Validate())
+
+	// relay MsgRecvPacket to osmosis, then MsgAcknowledgement back to gaia
+	require.NoError(t, r.Flush(ctx, eRep, ibcPath, gaiaChannelID))
+
+	// test source wallet has decreased funds
+	expectedBal := gaiaUserBalInitial.Sub(amountToSend)
+	gaiaUserBalNew, err := gaia.GetBalance(ctx, gaiaUser.FormattedAddress(), gaia.Config().Denom)
+	require.NoError(t, err)
+	require.True(t, gaiaUserBalNew.Equal(expectedBal))
+
+	// Trace IBC Denom
+	srcDenomTrace := transfertypes.ParseDenomTrace(transfertypes.GetPrefixedDenom("transfer", osmoChannelID, gaia.Config().Denom))
+	dstIbcDenom := srcDenomTrace.IBCDenom()
+
+	// Test destination wallet has increased funds
+	osmosUserBalNew, err := osmosis.GetBalance(ctx, osmosisUser.FormattedAddress(), dstIbcDenom)
+	require.NoError(t, err)
+	require.True(t, osmosUserBalNew.Equal(amountToSend))
+
+	// Validate light client
+	chain := osmosis.(*cosmos.CosmosChain)
+	reg := chain.Config().EncodingConfig.InterfaceRegistry
+	msg, err := cosmos.PollForMessage[*clienttypes.MsgUpdateClient](ctx, chain, reg, height, height+10, nil)
+	require.NoError(t, err)
+
+	require.Equal(t, "07-tendermint-0", msg.ClientId)
+	require.NotEmpty(t, msg.Signer)
 }
