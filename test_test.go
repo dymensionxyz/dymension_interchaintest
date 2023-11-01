2163150b58ecaf088fc9f3bb2e888abc32d735e1
Itay Levy <itaylevy@Itays-MacBook-Pro.local>
2023-11-01 12:42:23 +0200

    chains are starting successfully

--- test_test.go
+++ test_test.go
@@ -3,6 +3,7 @@ package dymension_interchaintest
 import (
 	"github.com/strangelove-ventures/interchaintest/v8"
 	"github.com/strangelove-ventures/interchaintest/v8/ibc"
+	"github.com/stretchr/testify/require"
 	"go.uber.org/zap/zaptest"
 	"testing"
 )
@@ -11,17 +12,29 @@ func TestLearn(t *testing.T) {
 	if testing.Short() {
 		t.Skip("skipping in short mode")
 	}
-
 	t.Parallel()
-
 	cf := interchaintest.NewBuiltinChainFactory(zaptest.NewLogger(t), []*interchaintest.ChainSpec{
 		{Name: "gaia", Version: "v7.0.0", ChainConfig: ibc.ChainConfig{
 			GasPrices: "0.0uatom",
 		}},
 		{Name: "osmosis", Version: "v11.0.0"},
 	})
-	_, err := cf.Chains(t.Name())
-	if err != nil {
+	chains, err := cf.Chains(t.Name())
+	require.NoError(t, err)
+	gaia, osmosis := chains[0], chains[1]
+	client, network := interchaintest.DockerSetup(t)
+	r := interchaintest.NewBuiltinRelayerFactory(ibc.CosmosRly, zaptest.NewLogger(t)).Build(
+		t, client, network)
+	const ibcPath = "gaia-osmo-demo"
+	_ = interchaintest.NewInterchain().
+		AddChain(gaia).
+		AddChain(osmosis).
+		AddRelayer(r, "relayer").
+		AddLink(interchaintest.InterchainLink{
+			Chain1:  gaia,
+			Chain2:  osmosis,
+			Relayer: r,
+			Path:    ibcPath,
+		})
 
-	}
 }
